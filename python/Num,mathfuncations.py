919a791629b4ceae98702cb747c922515573fb46
Macroshah <68460973+shahbasharat@users.noreply.github.com>
2023-02-17 21:56:19 +0530

    Delete python directory

--- python/Num,mathfuncations.py
+++
@@ -1,8 +0,0 @@
-x = 1      #integer
-y = 1.0    #float
-z = 1j     #complex
-print(x)
-print(y)
-print(z)
-a = int(y)
-print(a)
--- python/first.py
+++
@@ -1,6 +0,0 @@
-x = 7
-y = 9
-z = x+y
-a = "hello"
-print("hello")
-print(z)
